6bd0c002bc7fcdba4aa02bce61a3a872b71423c5
vidya <ksvidya1997@gmail.com>
2026-06-19 18:25:35 +0530

    Time: 48 ms (89.88%), Space: 54.1 MB (11.82%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -36,3 +36,41 @@ class BrowserHistory {
  * String param_2 = obj.back(steps);
  * String param_3 = obj.forward(steps);
  */
+
+
+//  class BrowserHistory {
+//     private Stack<String> history, future;
+//     private String current;
+
+//     public BrowserHistory(String homepage){
+//         history = new Stack<String>();
+//         future = new Stack<String>();
+//         current = homepage;
+//     }
+
+//     public void visit(String url){
+//         history.push(current);
+//         current = url;
+
+//         future = new Stack<String>();
+//     }
+
+//     public String back(int steps){
+//         while(steps > 0 && !history.isEmpty()){
+//             future.push(current);
+//             current = history.pop();
+//             steps--;
+//         }
+//         return current;
+//     }
+
+//     public String forward(int steps){
+//         while(steps > 0 && !future.isEmpty()){
+//             history.push(current);
+//             current = future.pop();
+//             steps--;
+//         }
+//         return current;
+        
+//     }
+//  }
